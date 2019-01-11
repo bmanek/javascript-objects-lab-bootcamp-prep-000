b6a5ca86e9a27f3c703d38a758fb464bbe293076
Ben Manekin <ben.manekin@gmail.com>
2019-01-11 22:48:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newThing = Object.assign({}, object, {[key]: [value]});
+  var newThing = Object.assign({}, object, {[key]: value});
   return newThing;
 }
 
