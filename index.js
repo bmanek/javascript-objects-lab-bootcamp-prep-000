4645cd11f92ad6759b3ac3fcc5113f2e2a9491eb
Ben Manekin <ben.manekin@gmail.com>
2019-01-11 22:26:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,8 @@ function deleteFromObjectByKey(object, key) {
   delete newObject.key;
   return object;
 }
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key;
+  return object;
+}
